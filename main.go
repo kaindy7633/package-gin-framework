64a609140381aee0104f0c7f839d1d93d288de6e
kaindy7633 <kaindy7633@gmail.com>
2021-11-24 16:06:09 +0800

    完成日志系统初始化配置

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kaindy7633/package-gin-framework/bootstrap"
+	"github.com/kaindy7633/package-gin-framework/global"
 )
 
 func main() {
+	// 初始化配置
+	bootstrap.InitializeConfig()
+
+	// 初始化日志
+	global.App.Log = bootstrap.InitializeLog()
+	global.App.Log.Info("log init success!")
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
-	_ = r.Run(":8080")
+	_ = r.Run(":" + global.App.Config.App.Port)
 }
